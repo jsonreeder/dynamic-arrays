1823da6020c18f9a4b55b096403a9a0f0d430e45
Jason Reeder <info@jasonreeder.com>
2017-04-12 11:09:15 -0700

    push/pop

--- lib/ring_buffer.rb
+++ lib/ring_buffer.rb
@@ -5,6 +5,7 @@ class RingBuffer
 
   def initialize
     @length = 0
+    @capacity = 0
   end
 
   # O(1)
@@ -14,14 +15,20 @@ class RingBuffer
 
   # O(1)
   def []=(index, val)
+    @store[index] = val
   end
 
   # O(1)
   def pop
+    @length -= 1
+    @store[@length]
   end
 
   # O(1) ammortized
   def push(val)
+    resize! if @length == @capacity
+    self[@length] = val
+    @length += 1
   end
 
   # O(1)
@@ -41,5 +48,9 @@ class RingBuffer
   end
 
   def resize!
+    @capacity *= 2
+    new_store = StaticArray.new(capacity)
+    @length.times { |idx| new_store[idx] = @store[idx] }
+    @store = new_store
   end
 end
